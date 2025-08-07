1d65efe42502040c8c0ac1225e2af32eddf53983
ChrisPagn <121812971+ChrisPagn@users.noreply.github.com>
2025-08-07 18:46:16 +0200

    mise a jour Librarycontext

--- Library81/Models/LibraryContext.cs
+++ Library81/Models/LibraryContext.cs
@@ -31,7 +31,12 @@ public partial class LibraryContext : DbContext
     public virtual DbSet<Subcategory> Subcategories { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code.
+//You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration -
+//see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings,
+//see https://go.microsoft.com/fwlink/?LinkId=723263.
+  
         => optionsBuilder.UseMySql("server=localhost;port=33020;database=library;user=root;password=password;treattinyasboolean=false", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)

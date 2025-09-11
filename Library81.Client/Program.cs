b0fd8ab66df69aa1d914b8696378e666b2de73ec
ChrisPagn <121812971+ChrisPagn@users.noreply.github.com>
2025-09-11 11:44:44 +0200

    bug

--- Library81.Client/Program.cs
+++ Library81.Client/Program.cs
@@ -9,17 +9,21 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddMudServices();
 
 // Add HttpClient
-builder.Services.AddSingleton(sp => {
-    var client = new HttpClient
-    {
-        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-    };
-    // Ajoutez des headers si nécessaire
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-    return client;
+//builder.Services.AddSingleton(sp => {
+//    var client = new HttpClient
+//    {
+//        BaseAddress = new Uri("https://localhost:7215") // Assure que le client pointe vers l'API serveur
+//    };
+//    client.DefaultRequestHeaders.Add("Accept", "application/json");
+//    return client;
+//});
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
 // Add API Service
 builder.Services.AddSingleton<IApiService, ApiService>();
 
-await builder.Build().RunAsync();
+await builder.Build().RunAsync();await builder.Build().RunAsync();
